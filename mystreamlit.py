8e0b14b2b63e212a6e98f19681e4058eca1bd8c3
Akash K P <kpakash03@gmail.com>
2023-04-13 12:04:35 +0530

    initial commit

---
+++ mystreamlit.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+
+URL1 = "http://127.0.0.1:5000/load"
+URL2 = "http://127.0.0.1:5000/getjson"
+
+st.write("running")
+uploaded_file = None
+# if st.button('Upload File'):
+uploaded_file = st.file_uploader("Choose a image file", type=["png", "jpg", "jpeg", "gif"])
+if uploaded_file is not None:
+    file = uploaded_file.read()
+    files = {'file': file}
+    r1 = requests.post(url=URL1, files=files)
+    # st.write(r1.json())
+    r = requests.get(url=URL2).json()
+
+    st.write("NAME :",r['name'])
+    st.write("AADHAAR NUMBER :",r['aadhaar_no'])
+    st.write("GENDER :",r['gender'])
+    st.write("DOB :",r['dob'])
+    st.write("ADDRESS :",r['address'])
+    st.write("ENROLLMENT NO :",r['enrollment_no'])
+    st.write("VID :",r['vid'])
+    st.write("PHONE NUMBER :",r['phonenumber'])
+    # st.write(r.)
+    # print(type(file))
+
+    st.write("------------------------------------------------------")
+    st.write("UPLOAD SUCCESSFULL")
+
+     #TODO able to upload front and back , both sides , and pdf files too
+
+# print(type(uploaded_file))
