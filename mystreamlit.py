e1b3402709ca15bc0a7e881d84b3d5c83000e883
Akash K P <kpakash03@gmail.com>
2023-04-25 11:13:32 +0530

    changed to include please wait animation

--- mystreamlit.py
+++ mystreamlit.py
@@ -9,11 +9,13 @@ uploaded_file = None
 # if st.button('Upload File'):
 uploaded_file = st.file_uploader("Choose a image file", type=["png", "jpg", "jpeg", "gif"])
 if uploaded_file is not None:
-    file = uploaded_file.read()
-    files = {'file': file}
-    r1 = requests.post(url=URL1, files=files)
-    # st.write(r1.json())
-    r = requests.get(url=URL2).json()
+    r = {}
+    with st.spinner('your file is processing...'):
+        file = uploaded_file.read()
+        files = {'file': file}
+        r1 = requests.post(url=URL1, files=files)
+        # st.write(r1.json())
+        r = requests.get(url=URL2).json()
 
     st.write("NAME :",r['name'])
     st.write("AADHAAR NUMBER :",r['aadhaar_no'])
